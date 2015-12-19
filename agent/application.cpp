32d820424ad7cf6cf164593e3a7596f0b670ce45
VK <kettunen.villu@gmail.com>
2015-12-20 00:48:35 +0300

    add agent src

--- agent/application.cpp
+++ agent/application.cpp
@@ -1,8 +1,21 @@
 #include "application.h"
+#include "vtrc-common/vtrc-pool-pair.h"
 
 namespace ta { namespace agent {
 
+    namespace {
+        namespace vcomm = vtrc::common;
+        typedef std::map<vcomm::rtti_wrapper, subsystem_sptr> subsys_map;
+        typedef std::vector<subsystem_sptr>                   subsys_vector;
+
+        struct subsystem_comtrainer {
+            subsys_map      subsys_;
+            subsys_vector   subsys_order_;
+        };
+    }
+
     struct application::impl {
+        subsystem_comtrainer     subsystems_;
         impl( )
         {
 
@@ -12,5 +25,35 @@ namespace ta { namespace agent {
     application::application( )
         :impl_(new impl)
     { }
+
+    void application::add_subsystem( const std::type_info &info,
+                                     subsystem_sptr inst )
+    {
+        impl_->subsystems_.subsys_[vcomm::rtti_wrapper(info)] = inst;
+        impl_->subsystems_.subsys_order_.push_back( inst );
+    }
+
+    subsystem_iface *application::subsystem( const std::type_info &info )
+    {
+        auto f = impl_->subsystems_.subsys_.find( vcomm::rtti_wrapper(info) );
+
+        if( f == impl_->subsystems_.subsys_.end( ) ) {
+            return NULL;
+        } else {
+            return f->second.get( );
+        }
+    }
+
+    const subsystem_iface *application::subsystem(
+                                             const std::type_info &info ) const
+    {
+        auto f = impl_->subsystems_.subsys_.find( vcomm::rtti_wrapper(info) );
+
+        if( f == impl_->subsystems_.subsys_.end( ) ) {
+            return NULL;
+        } else {
+            return f->second.get( );
+        }
+    }
 
 }}
--- agent/application.h
+++ agent/application.h
@@ -2,9 +2,13 @@
 #define TA_APPLICATION_CPP
 
 #include <memory>
+#include <assert.h>
 
 #include "vtrc-server/vtrc-application.h"
 #include "vtrc-common/vtrc-signal-declaration.h"
+#include "vtrc-common/vtrc-rtti-wrapper.h"
+
+#include "subsys-iface.h"
 
 namespace ta { namespace agent {
 
@@ -16,6 +20,79 @@ namespace ta { namespace agent {
         application & operator = ( application & ) = delete;
 
         application( );
+    private:
+
+        template <class Tgt, class Src>
+        static Tgt poly_downcast ( Src * x )
+        {
+            assert( dynamic_cast<Tgt>(x) == x );
+            return static_cast<Tgt>(x);
+        }
+
+    public: // subsystems
+
+        template <typename T>
+        void add_subsystem( )
+        {
+            subsystem_sptr subsys ( T::create( this ) );
+            add_subsystem( typeid( T ), subsys );
+        }
+
+        template <typename T, typename ... Args >
+        void add_subsystem( const Args & ... pars )
+        {
+            subsystem_sptr subsys ( T::create( this, pars ... ) );
+            add_subsystem( typeid( T ), subsys );
+        }
+
+        template <typename T>
+        T &subsystem( )
+        {
+            subsystem_iface *subsys = subsystem( typeid(T) );
+            if( NULL == subsys ) {
+                throw std::runtime_error( "Invalid subsystem" );
+            }
+            return *(poly_downcast<T *>( subsys ));
+        }
+
+        template <typename T>
+        const T &subsystem( ) const
+        {
+            const subsystem_iface *subsys = subsystem( typeid(T) );
+            if( NULL == subsys ) {
+                throw std::runtime_error( "Invalid subsystem" );
+            }
+            return *(poly_downcast<const T *>( subsys ));
+        }
+
+        template <typename T>
+        T *subsystem_safe( )
+        {
+            subsystem_iface *subsys = subsystem( typeid(T) );
+            return poly_downcast<const T *>( subsys );
+        }
+
+        template <typename T>
+        const T *subsystem_safe( ) const
+        {
+            const subsystem_iface *subsys = subsystem( typeid(T) );
+            return poly_downcast<const T *>( subsys );
+        }
+
+        void start_all( );
+        void stop_all( );
+
+    private:
+
+        void add_subsystem( const std::type_info &info, subsystem_sptr inst );
+
+        /* === nothrow === */
+        /*
+         * returns NULL if not found
+        */
+        subsystem_iface *subsystem( const std::type_info &info );
+        const subsystem_iface *subsystem( const std::type_info &info ) const;
+        /* =============== */
     };
 }}
 
