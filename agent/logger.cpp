98dd275183b02e6f8c3531a6e4551dcc78bd3108
VK <kettunen.villu@gmail.com>
2015-12-25 00:05:00 +0300

    logger

--- agent/logger.cpp
+++ agent/logger.cpp
@@ -38,7 +38,6 @@ namespace ta { namespace agent {
         impl_->dispatcher_.post( std::bind( &logger::do_write, this,
                                             lev, td.total_microseconds( ),
                                             data ) );
-
     }
 
     void logger::do_write( level lvl, std::uint64_t microsec,
