4622165163faeffe36b08a58ad9e3826b87316c7
newenclave <pikrat.acid@gmail.com>
2015-12-26 15:31:01 +0300

    client code

--- include/client-core/ta-client.h
+++ include/client-core/ta-client.h
@@ -1,7 +1,7 @@
 #ifndef TA_CLIENT_H
 #define TA_CLIENT_H
 
-
+#include "vtrc-common/vtrc-signal-declaration.h"
 #include "vtrc-common/vtrc-pool-pair.h"
 
 namespace vtrc { namespace client {
@@ -9,10 +9,13 @@ namespace vtrc { namespace client {
 }}
 
 namespace ta { namespace client {
+
     class core {
+
         struct         impl;
         friend struct  impl;
         impl          *impl_;
+
     public:
         core( vtrc::common::pool_pair &pp );
         ~core( );
