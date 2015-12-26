115d071b8b0217e45be71354f441b40716a25fdd
newenclave <pikrat.acid@gmail.com>
2015-12-26 15:17:57 +0300

    client code

--- client-core/ta-client.cpp
+++ client-core/ta-client.cpp
@@ -5,4 +5,19 @@
 
 namespace ta { namespace client {
 
+    struct core::impl: public vtrc::client::vtrc_client {
+        impl( vtrc::common::pool_pair &pp )
+            :vtrc::client::vtrc_client(pp)
+        { }
+    };
+
+    core::core( vtrc::common::pool_pair &pp )
+        :impl_(new impl(pp))
+    { }
+
+    core::~core( )
+    {
+        delete impl_;
+    }
+
 }}
--- include/client-core/ta-client.h
+++ include/client-core/ta-client.h
@@ -1,8 +1,22 @@
 #ifndef TA_CLIENT_H
 #define TA_CLIENT_H
 
-namespace ta { namespace client {
 
+#include "vtrc-common/vtrc-pool-pair.h"
+
+namespace vtrc { namespace client {
+    class vtrc_client;
+}}
+
+namespace ta { namespace client {
+    class core {
+        struct         impl;
+        friend struct  impl;
+        impl          *impl_;
+    public:
+        core( vtrc::common::pool_pair &pp );
+        ~core( );
+    };
 }}
 
 #endif // TACLIENT_H
