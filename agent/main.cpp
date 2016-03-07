c01c800fca9c03683e80cc32b7acb95883554388
VK <kettunen.villu@gmail.com>
2016-03-07 03:07:22 +0300

    add get_uuid for instances

--- agent/main.cpp
+++ agent/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 
+//#include <cstdint>
+//#include <iostream>
+
 #include "application.h"
 #include "google/protobuf/stubs/common.h"
 
+//template <typename T>
+//struct type_unique_id {
+//    typedef T type;
+//    static std::uintptr_t id( )
+//    {
+//        static const T * const i = nullptr;
+//        return reinterpret_cast<std::uintptr_t>(&i);
+//    }
+//};
+
 int main( int argc, const char *argv[ ] )
 {
+//    std::cout << type_unique_id<std::string>::id( ) << "\n";
+//    std::cout << type_unique_id<std::string>::id( ) << "\n";
+//    std::cout << type_unique_id<int>::id( ) << "\n";
+//    std::cout << type_unique_id<long>::id( ) << "\n";
+//    std::cout << type_unique_id<ta::agent::application>::id( ) << "\n";
+
+//    return 0;
     try {
         ta::agent::application tapp;
         tapp.run( argc, argv );
